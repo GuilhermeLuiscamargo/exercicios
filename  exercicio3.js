fbdd9d4c0a5482c6358fddd0d743bcefa6f6ea74
GuilhermeLuiscamargo <guilhermeluiscamargoneto@gmail.com>
2020-10-30 20:04:32 -0300

    exercicios de javascript 1 semana.

---
+++  exercicio3.js
@@ -0,0 +1,4 @@
+var numero=40;
+if(numero%2===0){
+console.log("o numero é par")}
+else{console.log("o numero é impar")}
---
+++ exercicio2.js
@@ -0,0 +1,5 @@
+var num1=1;
+var num2=1;
+if(num1==num2){
+console.log("os numeros são iguais")}
+else{console.log("os numeros são diferentes")}
